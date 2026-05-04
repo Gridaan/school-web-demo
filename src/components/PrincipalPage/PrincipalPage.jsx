a25841f0b930d9010578356b4699210f40a69df3
sudipto das <sudiptodas877@gmail.com>
2026-05-04 08:01:01 +0530

    chngs

--- src/components/PrincipalPage/PrincipalPage.jsx
+++ src/components/PrincipalPage/PrincipalPage.jsx
@@ -10,7 +10,7 @@ import PageFooterTag  from "../shared/PageFooterTag";
 export default function PrincipalPage() {
   return (
     <main className="bg-[var(--color-cream)] min-h-screen">
-      <TopBar currentPage="Principal's Message" />
+      
       <PrincipalIntro />
       <MessageBody />
       <PillarStrip />
